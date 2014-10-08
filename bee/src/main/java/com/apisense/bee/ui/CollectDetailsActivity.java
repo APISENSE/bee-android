af0eb9708f15a80ead365ca45fa2c08567b0be07
Antoine Veuiller <antoine.veuiller@inria.fr>
2014-10-08 15:30:23 +0200

    adding skeleton of bee ui

--- bee/src/main/java/com/apisense/bee/ui/CollectDetailsActivity.java
+++ bee/src/main/java/com/apisense/bee/ui/CollectDetailsActivity.java
@@ -1,23 +1,24 @@
-package com.apisense.bee;
+package com.apisense.bee.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.apisense.bee.R;
 
-public class BeeApplication extends Activity {
+public class CollectDetailsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bee_application);
+        setContentView(R.layout.activity_collect_details);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.bee_application, menu);
+        getMenuInflater().inflate(R.menu.collect_details, menu);
         return true;
     }
 
