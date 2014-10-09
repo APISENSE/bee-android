1015e7f74ba9a28e8988557c0f167f04fe7ff16a
Julien Duribreux <julien.duribreux@inria.fr>
2014-10-09 09:43:35 +0200

    Creating all fragment needed for slideShow activity

---
+++ bee/src/main/java/com/apisense/bee/ui/fragment/RegisterFragment.java
@@ -0,0 +1,29 @@
+package com.apisense.bee.ui.fragment;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.apisense.bee.R;
+
+public class RegisterFragment extends Fragment {
+
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+
+}
