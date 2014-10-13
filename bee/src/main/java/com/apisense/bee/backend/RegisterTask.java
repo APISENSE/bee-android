49a8f039e56003ca5fcc58af7e0769b8ef39aa7b
Julien Duribreux <julien.duribreux@inria.fr>
2014-10-13 11:54:23 +0200

    Implementing register feature

--- bee/src/main/java/com/apisense/bee/backend/RegisterTask.java
+++ bee/src/main/java/com/apisense/bee/backend/RegisterTask.java
@@ -4,30 +4,34 @@ import android.os.AsyncTask;
 import android.util.Log;
 import fr.inria.bsense.APISENSE;
 
-public class SignUpTask extends AsyncTask<String, Void, String> {
+public class RegisterTask extends AsyncTask<String, Void, String> {
     private final String TAG = this.getClass().getSimpleName();
     private AsyncTasksCallbacks listener;
-
-    public SignUpTask(AsyncTasksCallbacks listener) {
+    private final String DEFAULT_URL = "http://beta.apisense.io/hive";
+    public RegisterTask(AsyncTasksCallbacks listener) {
         this.listener = listener;
     }
 
     @Override
     protected String doInBackground(String... params) {
-        // params[0] == login
+        // params[0] == pseudo
         // params[1] == password
         // params[2] == URL hive (optionnal)
-        String email = params[0];
-        String password = params[1];
-        String apisenseUrl = (params.length >= 3) ? params[2] : "";
+        String pseudo = "";
+        String password = "";
+        String apisenseUrl = (params.length >= 3) ? params[2] : this.DEFAULT_URL;
 
         if (params[0].isEmpty() || params[1].isEmpty()) {
-            Log.e(TAG, "login or password is empty");
+            Log.e(TAG, "Login or password is empty");
             return "error";
+        } else {
+            pseudo = params[0];
+            password = params[1];
         }
+
         try {
-            APISENSE.apisServerService().createAccount(apisenseUrl, "", email, password, "");
-            APISENSE.apisServerService().connect(email, password);
+            APISENSE.apisServerService().createAccount(apisenseUrl, "", pseudo, password, "");
+            APISENSE.apisServerService().connect(pseudo, password);
         } catch (Exception e) {
             e.printStackTrace();
             return e.getMessage();
--- bee/src/main/java/com/apisense/bee/ui/fragment/RegisterFragment.java
+++ bee/src/main/java/com/apisense/bee/ui/fragment/RegisterFragment.java
@@ -1,16 +1,57 @@
 package com.apisense.bee.ui.fragment;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.EditText;
 import android.widget.TextView;
+import com.apisense.bee.backend.AsyncTasksCallbacks;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.widget.Button;
+import android.widget.EditText;
 import com.apisense.bee.R;
+import com.apisense.bee.backend.RegisterTask;
+import com.apisense.bee.ui.activity.HomeActivity;
+import fr.inria.bsense.APISENSE;
+import fr.inria.bsense.APISENSEListenner;
+import fr.inria.bsense.service.BeeSenseServiceManager;
 
 public class RegisterFragment extends Fragment {
+
+    private final String TAG = "Register fragment";
+
+    /**
+     * The default email to populate the email field with.
+     */
+    public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private RegisterTask mRegisterTask = null;
+
+    // Values for pseudo and password at the time of the login attempt.
+    private String mPseudo = "";
+    private String mPassword = "";
+    private String mPasswordRepeat = "";
+    private String mApisenseUrl = "";
+
+    // UI
+    private EditText mPseudoEditText;
+    private EditText mPasswordEditText;
+    private EditText mPasswordConfirmEditText;
+    private EditText mApisenseUrlEditText;
+    private TextView mApisenseHiveLabel;
+    private Button mRegisterButton;
+
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -20,9 +61,113 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View root = inflater.inflate(R.layout.fragment_register, container, false);
+
+        // Get all views
+        mPseudoEditText = (EditText) root.findViewById(R.id.registerPseudo);
+        mPasswordEditText = (EditText) root.findViewById(R.id.registerPassword);
+        mPasswordConfirmEditText = (EditText) root.findViewById(R.id.registerPasswordConfirm);
+        mApisenseHiveLabel = (TextView) root.findViewById(R.id.apisenseHive);
+        mApisenseUrlEditText = (EditText) root.findViewById(R.id.apisenseEditText);
+        mRegisterButton = (Button) root.findViewById(R.id.registerBtn);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+
+        mRegisterButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (View.VISIBLE == mApisenseUrlEditText.getVisibility()) {
+                    mApisenseUrlEditText.setVisibility(View.INVISIBLE);
+                    mApisenseHiveLabel.setVisibility(View.INVISIBLE);
+                } else {
+                    mApisenseUrlEditText.setVisibility(View.VISIBLE);
+                    mApisenseHiveLabel.setVisibility(View.VISIBLE);
+                }
+                return false;
+            }
+        });
+
+        // Set up Apisense
+        APISENSE.init(getActivity(), new APISENSEListenner() {
+            @Override
+            public void onConnected(BeeSenseServiceManager beeSenseServiceManager) {
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return root;
     }
 
+    /**
+     * Attempts to sign in or register the account specified by the login form. If there are form errors (invalid email, missing fields, etc.), the errors are presented and no actual login attempt is made.
+     */
+    public void attemptRegister() {
+        if (mRegisterTask != null)
+            return;
 
+        // Reset errors.
+        mPseudoEditText.setError(null);
+        mPasswordEditText.setError(null);
+        mPasswordConfirmEditText.setError(null);
+        mApisenseUrlEditText.setError(null);
+
+        // Store values at the time of the login attempt.
+        mPseudo = mPseudoEditText.getText().toString();
+        mPassword = mPasswordEditText.getText().toString();
+        mPasswordRepeat = mPasswordConfirmEditText.getText().toString();
+        mApisenseUrl = mApisenseUrlEditText.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password.
+        if (TextUtils.isEmpty(mPassword)) {
+            mPasswordEditText.setError(getString(R.string.error_field_required));
+            focusView = mPasswordEditText;
+            cancel = true;
+        } else if (mPassword.length() < 4) {
+            mPasswordEditText.setError(getString(R.string.signin_error_invalid_password));
+            focusView = mPasswordEditText;
+            cancel = true;
+        } else if (mPasswordRepeat.length() < 4 || !mPasswordRepeat.equals(mPassword)) {
+            mPasswordConfirmEditText.setError(getString(R.string.signup_error_invalid_repeat_password));
+            focusView = mPasswordConfirmEditText;
+            cancel = true;
+        } else if (TextUtils.isEmpty(mApisenseUrl)) {
+            mApisenseUrlEditText.setError(getString(R.string.error_field_required));
+            focusView = mApisenseUrlEditText;
+            cancel = true;
+        }
+
+        if (cancel)
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            mRegisterTask = new RegisterTask(new AsyncTasksCallbacks() {
+                @Override
+                public void onTaskCompleted(Object response) {
+                    Log.i(TAG, "Register result: " + response);
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(getActivity(), HomeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+                @Override
+                public void onTaskCanceled() {
+
+                }
+            });
+
+            mRegisterTask.execute(mPseudo, mPassword, mApisenseUrl);
+        }
+    }
 }
