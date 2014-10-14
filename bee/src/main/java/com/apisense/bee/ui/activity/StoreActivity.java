0a36729f558ca352232d5ce088a9346cda8dbe25
Antoine Veuiller <antoine.veuiller@inria.fr>
2014-10-14 16:45:53 +0200

    Addind Experiment Store first version

--- bee/src/main/java/com/apisense/bee/ui/activity/StoreActivity.java
+++ bee/src/main/java/com/apisense/bee/ui/activity/StoreActivity.java
@@ -1,36 +1,129 @@
 package com.apisense.bee.ui.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import com.apisense.bee.BeeApplication;
 import com.apisense.bee.R;
+import com.apisense.bee.backend.AsyncTasksCallbacks;
+import com.apisense.bee.backend.experiment.RetrieveAvailableExperimentsTask;
+import com.apisense.bee.backend.store.RetrieveExistingTagsTask;
+import com.apisense.bee.ui.adapter.SubscribedExperimentsListAdapter;
+import fr.inria.bsense.appmodel.Experiment;
+
+import java.util.ArrayList;
+import java.util.List;
+// TODO: Think about index usage (when to load next 'page' of experiment)
+// TODO: Think about Tags usage (Server filter is better)
 
 public class StoreActivity extends Activity {
+    private final String TAG = getClass().getSimpleName();
+
+    protected  ActionBar actionBar;
+
+    // Content Adapter
+    // TODO: Make a specific adapter when presentation will diverge
+    protected SubscribedExperimentsListAdapter experimentsAdapter;
+
+    // Asynchronous Task
+    private RetrieveAvailableExperimentsTask experimentsRetrieval;
+    private RetrieveExistingTagsTask tagsRetrieval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+        actionBar = getActionBar();
+        experimentsAdapter = new SubscribedExperimentsListAdapter(getBaseContext(),
+                                                                  R.layout.fragment_experimentelement,
+                                                                  new ArrayList<Experiment>());
+        ListView subscribedExperiments = (ListView) findViewById(R.id.store_experiment_lists);
+        subscribedExperiments.setAdapter(experimentsAdapter);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.store, menu);
+        // Inflate the ActionBar and the tabs.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        populateTabs();
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    private void populateTabs() {
+        if (tagsRetrieval == null){
+            tagsRetrieval = new RetrieveExistingTagsTask(new onExistingTagsRetrieved());
+            tagsRetrieval.execute();
         }
-        return super.onOptionsItemSelected(item);
     }
+
+    public void setExperiments(List<Experiment> experiments) {
+        this.experimentsAdapter.setDataSet(experiments);
+    }
+
+    private class onExperimentsRetrieved implements AsyncTasksCallbacks {
+        @Override
+        public void onTaskCompleted(int result, Object response) {
+            experimentsRetrieval = null;
+            List<Experiment> exp = (List<Experiment>) response;
+            Log.i(TAG, "number of Active Experiments: " + exp.size());
+
+            // Updating listview
+            if (exp.size() != 0) {
+                setExperiments(exp);
+                experimentsAdapter.notifyDataSetChanged();
+            }
+        }
+
+        @Override
+        public void onTaskCanceled() {
+            experimentsRetrieval = null;
+        }
+    }
+
+    private class onExistingTagsRetrieved implements AsyncTasksCallbacks {
+        @Override
+        public void onTaskCompleted(int result, Object response) {
+            tagsRetrieval = null;
+            if ((Integer) result == BeeApplication.ASYNC_SUCCESS) {
+                List<String> tags = (List<String>) response;
+                Log.d(TAG, "Fetched tags: " + tags);
+                for(String tag : tags){
+                    Log.v(TAG, "Adding tab for tag: " + tag);
+                    actionBar.addTab(actionBar.newTab()
+                                     .setText(tag)
+                                     .setTabListener(new TagTabListener()));
+                }
+            }
+        }
+
+        @Override
+        public void onTaskCanceled() {
+            tagsRetrieval = null;
+        }
+    }
+
+   private class TagTabListener implements ActionBar.TabListener {
+       public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+           // TODO: Filter retrieved experiments by Tag
+           if (experimentsRetrieval != null) {
+               experimentsRetrieval.cancel(true);
+           } else {
+               experimentsRetrieval = new RetrieveAvailableExperimentsTask(new onExperimentsRetrieved());
+               experimentsRetrieval.execute();
+           }
+       }
+
+       public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+           // hide the given tab
+       }
+
+       public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+           // probably ignore this event
+       }
+   }
 }
