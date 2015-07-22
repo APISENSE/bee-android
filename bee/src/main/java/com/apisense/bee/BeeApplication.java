de34c7762c4c65b831a5c55144e0f233fd711e6e
Antoine Veuiller <antoine.veuiller@inria.fr>
2015-07-22 17:25:49 +0200

    fix(Rollbar): Change token to the good one.

--- bee/src/main/java/com/apisense/bee/BeeApplication.java
+++ bee/src/main/java/com/apisense/bee/BeeApplication.java
@@ -20,7 +20,7 @@ public class BeeApplication extends Application {
                 "https://spoonware.lille.inria.fr/rest",
                 "bee-3cbc5f3d-4f78-4b2b-a110-38297867726b",  // Application ID
                 "8up76ft1kkb1kqd49f20ismub2");  // Application TOKEN
-        Rollbar.init(this, "2b8b039c69764b36ab2788128c011383", "production");
+        Rollbar.init(this, "61805ed6db4d4a12832bc706019eeb1e", "production");
     }
 
 }
