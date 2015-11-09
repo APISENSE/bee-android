65974ffea6e6e7d1a5f50365387eb098d02c63f8
Antoine Veuiller <antoine.veuiller@inria.fr>
2015-11-09 17:43:44 +0100

    chores(version): Updated Google play services version
    Also delete spoonware from project.

--- bee/src/main/java/com/apisense/bee/BeeApplication.java
+++ bee/src/main/java/com/apisense/bee/BeeApplication.java
@@ -19,11 +19,6 @@ public class BeeApplication extends Application {
         sdk = new APISENSE(this).enableGCM(getString(R.string.gcm_defaultSenderId))
                 .getSdk();
 
-        fr.spoonware.SpoonwareAndroid.init(this,
-                "https://spoonware.lille.inria.fr/rest",
-                "bee-3cbc5f3d-4f78-4b2b-a110-38297867726b",  // Application ID
-                "8up76ft1kkb1kqd49f20ismub2");  // Application TOKEN
-
         Rollbar.init(this, "61805ed6db4d4a12832bc706019eeb1e", "production");
     }
 
